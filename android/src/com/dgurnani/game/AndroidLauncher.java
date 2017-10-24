b22f04820c9a62df6a64de2bff7e4696beccf284
dgurnani <gurnani.dinesh@gmail.com>
2017-10-24 19:25:11 -0400

    Initial commit

---
+++ android/src/com/dgurnani/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dgurnani.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dgurnani.game.FlappyBirdie;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBirdie(), config);
+	}
+}
