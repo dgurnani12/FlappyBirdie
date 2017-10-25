cd7492aa849d417d2f0b1c054a1c4835a7731bd6
dgurnani <gurnani.dinesh@gmail.com>
2017-10-25 19:21:46 -0400

    added collision system

--- core/src/com/dgurnani/game/FlappyBirdie.java
+++ core/src/com/dgurnani/game/FlappyBirdie.java
@@ -2,9 +2,14 @@ package com.dgurnani.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 
 import java.util.Random;
 
@@ -23,6 +28,8 @@ public class FlappyBirdie extends ApplicationAdapter {
     private float Y_Velocity = 0; // Y-axis velocity (+ is downward)
     private float Y_Gravity = 2; // Y-axis gravitational acceleration (+ is downward)
 
+    private Circle birdShadow; // Circle - an easy way to shadow the bird
+
     private Texture topTube;
     private Texture bottomTube;
 
@@ -30,17 +37,23 @@ public class FlappyBirdie extends ApplicationAdapter {
     private float maxTubeDisplacement;
     private Random randomGenerator;
     private float tubeVelocity = 4; // (+ is leftward)
-    private int numberOfTubesSets = 4;
+    private int numberOfTubeSets = 4;
     private float distanceBetweenTubes;
 
-    private float[] tubeX = new float[numberOfTubesSets]; // All tube sets positions on X axis
-    private float[] tubeOffset = new float[numberOfTubesSets]; // Y axis offsets of the tube sets
+    private float tubeXSync;
+    private float[] tubeX = new float[numberOfTubeSets]; // All tube sets positions on X axis
+    private float[] tubeOffset = new float[numberOfTubeSets]; // Y axis offsets of the tube sets
+
+    private Rectangle[] topTubeShadows;
+    private Rectangle[] bottomTubeShadows;
 
     @Override
 	public void create () {
 
         batch = new SpriteBatch();
 		background = new Texture("background.png");
+
+        birdShadow = new Circle();
 
 		birds = new Texture[3];
 		birds[0] = new Texture("Bird-1.png");
@@ -60,10 +73,19 @@ public class FlappyBirdie extends ApplicationAdapter {
 
         distanceBetweenTubes = Gdx.graphics.getWidth() * 3 / 4;
 
+        topTubeShadows = new Rectangle[numberOfTubeSets];
+        bottomTubeShadows = new Rectangle[numberOfTubeSets];
+
         // Setting the X-axis position of all tubes and there respective offsets
-        for (int i = 0; i < numberOfTubesSets; i++) {
+        for (int i = 0; i < numberOfTubeSets; i++) {
             tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * 2 * maxTubeDisplacement;
+
+            tubeXSync = (Gdx.graphics.getWidth() / 2) + (topTube.getWidth() / 2);
             tubeX[i] = Gdx.graphics.getWidth() / 2 - topTube.getWidth() / 2 + i * distanceBetweenTubes;
+            tubeX[i] += tubeXSync;
+
+            topTubeShadows[i] = new Rectangle();
+            bottomTubeShadows[i] = new Rectangle();
         }
 	}
 
@@ -80,11 +102,11 @@ public class FlappyBirdie extends ApplicationAdapter {
             }
 
             // update the 4 sets of tubes
-            for (int i = 0; i < numberOfTubesSets; i++) {
+            for (int i = 0; i < numberOfTubeSets; i++) {
                 if (tubeX[i] < - topTube.getWidth()) {
                     // off screen and moving away case
                     tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * 2 * maxTubeDisplacement;
-                    tubeX[i] += numberOfTubesSets * distanceBetweenTubes;
+                    tubeX[i] += numberOfTubeSets * distanceBetweenTubes;
                 } else {
                     // on screen or moving towards case
                     tubeX[i] = tubeX[i] - tubeVelocity;
@@ -92,6 +114,10 @@ public class FlappyBirdie extends ApplicationAdapter {
 
                 batch.draw(topTube, tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i]);
                 batch.draw(bottomTube, tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i]);
+
+                // shadows
+                topTubeShadows[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(), topTube.getHeight());
+                bottomTubeShadows[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i], bottomTube.getWidth(), bottomTube.getHeight());
             }
 
             // position update system
@@ -119,6 +145,15 @@ public class FlappyBirdie extends ApplicationAdapter {
 		imageUpdateCounter++;
 
 		batch.end();
+
+        birdShadow.set(Gdx.graphics.getWidth() / 2, Y_Position + birds[flapState].getHeight() / 2, birds[flapState].getWidth() / 2);
+
+        for (int i = 0; i < numberOfTubeSets; i++) {
+            if (Intersector.overlaps(birdShadow, topTubeShadows[i]) || Intersector.overlaps(birdShadow, bottomTubeShadows[i])) {
+                Gdx.app.log("Collision", "Yes!");
+            }
+        }
+
 	}
 	
 	@Override
