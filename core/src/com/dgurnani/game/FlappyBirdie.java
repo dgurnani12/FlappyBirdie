a83d9513999f85c1491a3921f202df7bf473f08e
dgurnani <gurnani.dinesh@gmail.com>
2017-10-24 20:53:03 -0400

    build version #1

--- core/src/com/dgurnani/game/FlappyBirdie.java
+++ core/src/com/dgurnani/game/FlappyBirdie.java
@@ -7,27 +7,67 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBirdie extends ApplicationAdapter {
+
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture background;
+
+	Texture[] birds; // holds 3 birds states to create motion
+	int flapState = 0; // index for  image state for flappy bird
+
+	int imageUpdateCounter = 0;
+
+	float Y_Position = 0; // Y-axis position
+	float Y_Velocity = 0; // Y-axis velocity
+	float Y_Gravity = 2; // Y-axis gravitational acceleration
+
+	int gameState = 0; // state of the game (did user touch the screen)
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("background.png");
+
+		birds = new Texture[3];
+		birds[0] = new Texture("Bird-1.png");
+		birds[1] = new Texture("Bird-2.png");
+		birds[2] = new Texture("Bird-3.png");
+
+		// Gets the bird exactly at the center
+		Y_Position = Gdx.graphics.getHeight() / 2 - birds[0].getHeight() / 2;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		// draw background:
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		// Draw bird:
+		if(imageUpdateCounter % 10 == 0) {
+			flapStateConfig();
+		}
+		batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, Y_Position);
+		imageUpdateCounter++;
+
+		
+
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+	}
+
+	// return the next flapState in the sequence 0,1,2 ...
+	private void flapStateConfig() {
+		if (flapState == 0) {
+			flapState = 1;
+		} else if(flapState == 1) {
+			flapState = 2;
+		} else {
+			flapState = 0;
+		}
 	}
 }
